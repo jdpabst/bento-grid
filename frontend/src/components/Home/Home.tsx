36004217ecf01a1cbedb54b5dc84d7ab4f7c0e78
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2024-10-03 14:22:31 -0600

    finished with desktop and moobile

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
    <ul className='list-container'>
 
     <li className='group-1'>
-     <div className='two'>
+     <div className='two margin-bottom-mobile'>
       <div className='section-2'>
        <h1>Social Media <span style={{ color: 'hsl(39, 100%, 71%)' }}>10x</span> <span style={{ 'fontStyle': 'italic' }}>Faster</span> with AI</h1>
        <img src='/assets/images/illustration-five-stars.webp' />
@@ -16,20 +16,20 @@ export default function Home() {
       </div>
      </div>
      <div className='group-1-subgroup'>
-      <div className='five'>
+      <div className='five margin-bottom-mobile'>
        <div className='section-5'>
         <img src='/assets/images/illustration-multiple-platforms.webp' />
         <h1>Manage multiple accounts and platforms.</h1>
        </div>
       </div>
-      <div className='seven'>
+      <div className='seven margin-bottom-mobile'>
        <div className='section-7'>
         <h1>Maintain a consistent posting schedule.</h1>
         <img src='/assets/images/illustration-consistent-schedule.webp' />
        </div>
       </div>
      </div>
-     <div className='three'>
+     <div className='three margin-bottom-mobile'>
       <div className='section-3'>
        <h1>Schedule to social media.</h1>
        <img src='/assets/images/illustration-schedule-posts.webp' />
@@ -39,14 +39,14 @@ export default function Home() {
     </li>
 
     <li className='group-2'>
-     <div className='six'>
+     <div className='six margin-bottom-mobile'>
       <div className='section-6'>
        <h1>{'>'}56%</h1>
        <p>faster audience growth</p>
        <img src='/assets/images/illustration-audience-growth.webp' />
       </div>
      </div>
-     <div className='eight'>
+     <div className='eight margin-bottom-mobile'>
       <div className='section-8'>
        <img src='/assets/images/illustration-grow-followers.webp' />
        <h1>Grow followers with non-stop content.</h1>
@@ -55,13 +55,13 @@ export default function Home() {
     </li>
 
     <li className='group-3'>
-     <div className='one'>
+     <div className='one margin-bottom-mobile'>
       <div className='section-1'>
        <h1>Create and schedule content <span style={{ color: 'hsl(256, 67%, 59%)' }}>quicker.</span></h1>
        <img src='/assets/images/illustration-create-post.webp' />
       </div>
      </div>
-     <div className='four'>
+     <div className='four margin-bottom-mobile'>
       <div className='section-4'>
        <h1>Write your content using AI.</h1>
        <img src='/assets/images/illustration-ai-content.webp' />
